f87380e52b501cd5a1eec86710468b1bf5d69093
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-07-05 13:51:43 -0700

    Add files via upload

---
+++ PS-Graph_O365_UserLogonReport.ps1
@@ -0,0 +1,36 @@
+# PS-Graph_O365_UserLogonReport
+
+# List Office 365 Logins via MS Graph
+
+#Connect to Microsoft Graph
+Connect-MGGraph -Environment USGov -Scopes "AuditLog.Read.All","User.Read.All" 
+ 
+#Set the Graph Profile
+Select-MgProfile beta
+ 
+#Properties to Retrieve
+$Properties = @(
+    'Id','DisplayName','Mail','UserPrincipalName','AccountEnabled', 'SignInActivity'   
+)
+ 
+#Get All users along with the properties
+$AllUsers = Get-MgUser -All -Property $Properties #| Select-Object $Properties
+ 
+$SigninLogs = @()
+ForEach ($User in $AllUsers)
+{
+    $SigninLogs += [PSCustomObject][ordered]@{
+            LoginName       = $User.UserPrincipalName
+            Email           = $User.Mail
+            DisplayName     = $User.DisplayName
+            AccountEnabled  = $User.AccountEnabled
+            LastSignIn      = $User.SignInActivity.LastSignInDateTime
+    }
+}
+ 
+$SigninLogs
+ 
+#Export Data to CSV
+$SigninLogs | Export-Csv -Path "C:\Temp\SigninLogs.csv" -NoTypeInformation
+
+#Orign / Source: https://www.sharepointdiary.com/2022/04/office-365-find-last-login-date-using-powershell.html
